5f8142a5065b816b0d7c9c6a07170461a41c69ca
umemak <umematsu@gmail.com>
2022-02-25 23:06:34 +0900

    divide main

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,21 @@ use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-    let mut f = File::open(filename).expect("file not found");
+    let config = parse_config(&args);
+    let mut f = File::open(config.filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
     println!("{}", contents);
+}
+
+struct Config {
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config{
+    let filename = args[1].clone();
+
+    Config { filename }
 }
